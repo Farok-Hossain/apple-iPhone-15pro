f4869e4e496b1584fb97a8835ce4f3882572615c
Farok Hossain <farokafs7@gmail.com>
2024-03-27 16:51:19 +0600

    video animation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,49 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else setVideoSrc(heroVideo);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to(".hero-title", { opacity: 1, delay: 1.5 });
+  }, []);
+  return (
+    <section className="w-full  nav-height relative bg-black">
+      <div className=" h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className=" pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
