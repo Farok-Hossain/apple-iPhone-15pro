a59f2fe60c77ae8e6a9defaa62be6ab6751485ff
Farok Hossain <farokafs7@gmail.com>
2024-03-27 20:20:53 +0600

    hero component updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,8 @@ const Hero = () => {
   }, []);
 
   useGSAP(() => {
-    gsap.to(".hero-title", { opacity: 1, delay: 1.5 });
+    gsap.to(".hero-title", { opacity: 1, delay: 2 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 2 });
   }, []);
   return (
     <section className="w-full  nav-height relative bg-black">
@@ -41,6 +42,15 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div
+        id="cta"
+        className=" flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className=" btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
