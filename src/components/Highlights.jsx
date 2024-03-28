1aa1e3a4a680b9da36fd5569cab8e9cfbdba9493
Farok Hossain <farokafs7@gmail.com>
2024-03-28 16:38:39 +0600

    highlights component added

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,18 @@
 const Highlights = () => {
-  return <div>Highlights</div>;
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
