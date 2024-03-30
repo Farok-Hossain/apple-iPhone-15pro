284074cab58031129bf86b0ea8b26fd802b54e55
Farok Hossain <farokafs7@gmail.com>
2024-03-31 04:09:00 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
+import * as Sentry from "@sentry/react";
+
 const App = () => {
   return (
     <main className=" bg-black">
@@ -14,4 +16,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Sentry.withProfiler(App);
