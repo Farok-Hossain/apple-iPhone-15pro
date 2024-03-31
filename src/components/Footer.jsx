a36fd0cbe7a74723c830af25fb2061b690728e5c
Farok Hossain <farokafs7@gmail.com>
2024-04-01 01:52:59 +0600

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,17 @@ const Footer = () => {
           <p className="font-semibold text-gray text-xs">
             More ways to shop:{" "}
             <span className="underline text-blue">Find an Apple Store{""}</span>
-            or
-            <span className="underline text-blue">other retailer</span>
-            near you.
+            or <span className="underline text-blue"> other retailer</span> near
+            you.
           </p>
+          <p className="font-semibold text-gray text-xs">Or call 01618603009</p>
+        </div>
+        <div className=" bg-neutral-700 my-5 h-[1px] w-full" />
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+          <p className="font-semibold text-gray text-xs">
+            Copyright @ 2024 Apple Inc. All rights reserved.
+          </p>
+          <div></div>
         </div>
       </div>
     </footer>
